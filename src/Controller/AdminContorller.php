941114e998a72226ea521b106d57449a135b45d0
casper <302582236@student.rocmondriaan.nl>
2020-12-14 14:56:56 +0100

    added kan medicijnen laten zien

--- src/Controller/AdminContorller.php
+++ src/Controller/AdminContorller.php
@@ -2,17 +2,26 @@
 
 
 namespace App\Controller;
+
+use App\Entity\Medicijnen;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-
 
 class AdminContorller extends AbstractController
 {
     /**
-     * @route("/medicijnen/lijst", name="lijstmedicijnen")
+     * @Route("/medicijnen/lijst", name="listmedicijnen")
      */
-    public function showMedicijnenAction(){
-        return $this->render('medicijnen/show.html.twig');
+    public function showMedicijnenAction(EntityManagerInterface $entitymanager){
+        $medicijnen = $entitymanager->getRepository(Medicijnen::class)->findAll();
+        return $this->render('medicijnen/show.html.twig',[
+            'medicijnen'=> $medicijnen
+        ]);
+    }
+    public function addMedicijnACtion(){
+
     }
 }
